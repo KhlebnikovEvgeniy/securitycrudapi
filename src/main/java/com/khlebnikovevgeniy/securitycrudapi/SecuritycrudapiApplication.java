2a82eb90c678c959258d5e9328cca25544015b81
KhlebnikovEvgeniy <khebnikove@gmail.com>
2025-04-04 21:37:23 +0300

    Initial Commit

---
+++ src/main/java/com/khlebnikovevgeniy/securitycrudapi/SecuritycrudapiApplication.java
@@ -0,0 +1,13 @@
+package com.khlebnikovevgeniy.securitycrudapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuritycrudapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecuritycrudapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/khlebnikovevgeniy/securitycrudapi/SecuritycrudapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khlebnikovevgeniy.securitycrudapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuritycrudapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
