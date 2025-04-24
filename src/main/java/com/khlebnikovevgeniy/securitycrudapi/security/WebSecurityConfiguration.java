8fa8fac5112604baab1e1b30a9b2c1d9a7fbffbb
KhlebnikovEvgeniy <khebnikove@gmail.com>
2025-04-24 20:42:04 +0300

    Web Security Configuration

---
+++ src/main/java/com/khlebnikovevgeniy/securitycrudapi/security/WebSecurityConfiguration.java
@@ -0,0 +1,36 @@
+package com.khlebnikovevgeniy.securitycrudapi.security;
+
+import static org.springframework.http.HttpMethod.POST;
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+@EnableMethodSecurity(securedEnabled = true, prePostEnabled = true)
+public class WebSecurityConfiguration {
+	
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		AuthenticationManagerBuilder authenticationManagerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
+		authenticationManagerBuilder.authenticationProvider(null);
+		http.csrf(csrf -> csrf.disable());
+		http.authorizeHttpRequests((authz) -> authz
+			        .requestMatchers(POST, "/api/accounts/**").hasAnyRole("ADMIN", "USER")
+			        .anyRequest().authenticated())
+					.httpBasic(Customizer.withDefaults())
+					.sessionManagement(session -> session.sessionCreationPolicy(STATELESS));	
+		return http.build();
+	}
+	
+}
