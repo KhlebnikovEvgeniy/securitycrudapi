850272fd6584640bc8c8e6a0d86a44df267bccd4
KhlebnikovEvgeniy <khebnikove@gmail.com>
2025-05-02 13:51:19 +0300

    WebSecurity Configuration

--- src/main/java/com/khlebnikovevgeniy/securitycrudapi/security/WebSecurityConfiguration.java
+++ src/main/java/com/khlebnikovevgeniy/securitycrudapi/security/WebSecurityConfiguration.java
@@ -1,12 +1,11 @@
 package com.khlebnikovevgeniy.securitycrudapi.security;
 
 import static org.springframework.http.HttpMethod.POST;
+import static org.springframework.security.config.Customizer.withDefaults;
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -23,15 +22,20 @@ public class WebSecurityConfiguration {
 	
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		AuthenticationManagerBuilder authenticationManagerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
-		authenticationManagerBuilder.authenticationProvider(accountAuthenticationProvider);
-		http.csrf(csrf -> csrf.disable());
-		http.authorizeHttpRequests((authz) -> authz
-			        .requestMatchers(POST, "/api/accounts/**").hasAnyRole("ADMIN", "USER")
-			        .anyRequest().authenticated())
-					.httpBasic(Customizer.withDefaults())
-					.sessionManagement(session -> session.sessionCreationPolicy(STATELESS));	
-		return http.build();
+		 http.csrf(csrf -> csrf.disable())
+         .authorizeHttpRequests()
+         .requestMatchers(POST, "/api/accounts/**")
+         .permitAll()
+         .requestMatchers("/**")
+         .authenticated()
+         .anyRequest()
+         .hasAnyRole("USER", "ADMIN")
+         .and()
+         .authenticationProvider(accountAuthenticationProvider)
+         .httpBasic(withDefaults())
+         .sessionManagement()
+         .sessionCreationPolicy(STATELESS);
+		 return http.build();
 	}
 		
 }
