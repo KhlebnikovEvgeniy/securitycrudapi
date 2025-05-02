8bd98b1029bc1d1c2c241d171c5b7724e087a60d
KhlebnikovEvgeniy <khebnikove@gmail.com>
2025-05-02 18:28:19 +0300

    Employee Model

--- src/main/java/com/khlebnikovevgeniy/securitycrudapi/model/Employee.java
+++ src/main/java/com/khlebnikovevgeniy/securitycrudapi/model/Employee.java
@@ -1,6 +1,7 @@
 package com.khlebnikovevgeniy.securitycrudapi.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -16,6 +17,7 @@ import lombok.Setter;
 @Table(name = "employees")
 public class Employee {
 	@Id
+	@GeneratedValue
 	private Integer id;
 	private String firstName;
 	private String lastName;
