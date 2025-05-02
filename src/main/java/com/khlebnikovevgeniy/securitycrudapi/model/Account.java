568744d28460a8fc7f1822592eae8d478277c1b9
KhlebnikovEvgeniy <khebnikove@gmail.com>
2025-05-02 13:51:56 +0300

    Account Model

--- src/main/java/com/khlebnikovevgeniy/securitycrudapi/model/Account.java
+++ src/main/java/com/khlebnikovevgeniy/securitycrudapi/model/Account.java
@@ -10,6 +10,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
@@ -29,6 +30,7 @@ import lombok.Setter;
 @Table(name = "accounts")
 public class Account {
 	@Id
+	@GeneratedValue
 	private Long id;
 	@Column(unique = true)
     @NotNull
